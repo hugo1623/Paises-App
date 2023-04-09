cdca201ab4e9b7f9b3f43e0bbf60ea3e64e3e998
Hugo Luis <hugoluis.system@gmail.com>
2023-04-08 21:27:28 -0500

    Componente Input y Tabla

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -7,6 +7,8 @@ import { PorCapialComponent } from './pages/por-capial/por-capial.component';
 import { PorPaisComponent } from './pages/por-pais/por-pais.component';
 import { PorRegionComponent } from './pages/por-region/por-region.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
+import { PaisTablaComponent } from './components/pais-tabla/pais-tabla.component';
+import { PaisInputComponent } from './components/pais-input/pais-input.component';
 
 
 @NgModule({
@@ -14,7 +16,9 @@ import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
     PorCapialComponent,
     PorPaisComponent,
     PorRegionComponent,
-    VerPaisComponent
+    VerPaisComponent,
+    PaisTablaComponent,
+    PaisInputComponent
   ],
   exports:[
     PorCapialComponent,
