a32b9962138f3b497c5f17a413c795305f99dcac
Hugo Luis <hugoluis.system@gmail.com>
2023-04-08 20:33:43 -0500

    Llenando Tabla de Países

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule } from '@angular/router';
 // Componentes
 import { PorCapialComponent } from './pages/por-capial/por-capial.component';
 import { PorPaisComponent } from './pages/por-pais/por-pais.component';
 import { PorRegionComponent } from './pages/por-region/por-region.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
+
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    RouterModule
   ]
 })
 export class PaisModule { }
