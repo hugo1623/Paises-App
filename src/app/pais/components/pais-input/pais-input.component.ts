71002517e3e8a13877bc8aab7e23b1ccb5d7bba1
Hugo Luis <hugoluis.system@gmail.com>
2023-04-09 23:57:14 -0500

    Debounce en el Input

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,18 +1,33 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-pais-input',
   templateUrl: './pais-input.component.html',
   styleUrls: ['./pais-input.component.css']
 })
-export class PaisInputComponent {
+export class PaisInputComponent implements OnInit{
+
 
 @Output() onEnter: EventEmitter<string> = new EventEmitter();
+@Output() onDebounce: EventEmitter<string>= new EventEmitter();
+
+debouncer: Subject<string>= new Subject();
 
 termino : string = '';
 
+ngOnInit(){
+  this.debouncer
+  .pipe(debounceTime(300))
+  .subscribe(valor => {
+    this.onDebounce.emit(valor);
+  });
+}
 buscar(){
   this.onEnter.emit(this.termino);
+}
+teclaPrecionada(){
+  this.debouncer.next(this.termino);
 }
 
 }
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -18,7 +18,6 @@ export class PorPaisComponent {
 
     this.hayError = false;
     this.termino = termino;
-    console.log(this.termino);
 
     this.paisService.buscarPais(this.termino)
     .subscribe((paises) => {
@@ -27,7 +26,10 @@ export class PorPaisComponent {
       }, (err) => {
         this.hayError = true;
         this.paises = [];
-      }
-    );
+      });
+  }
+  sugerencia(termino:string){
+    this.hayError = false;
+    // sugerencias-->crear
   }
 }
