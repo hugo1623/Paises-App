eb5957bfac533669e621cf11453101cc72cdce74
Hugo Luis <hugoluis.system@gmail.com>
2023-04-11 03:53:53 -0500

    Fin de la pantalla verPais

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,52 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
-import { switchMap,tap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
 import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styleUrls: ['./ver-pais.component.css']
+  styleUrls: ['./ver-pais.component.css'],
 })
 export class VerPaisComponent {
   pais!: Country;
 
-  constructor( 
-    private activatedRoute: ActivatedRoute,
-    private paisService: PaisService
-    ) {
-      activatedRoute.params.subscribe(data => {
-        // console.log('Este es el ID:' + data['cca2']);
+  constructor(private activatedRoute: ActivatedRoute, private paisService: PaisService) { 
+    activatedRoute.params.subscribe((data) => {
       this.getPaisPorAlpha(data['cca2']);
-      })
-     }
-      getPaisPorAlpha(cca2: string){
-        this.paisService.getPaisPorAlpha(cca2).subscribe(data=>{
-          console.log(data);
-          this.pais= data;
-        });
+    });
+  }
 
-      }
-  // ngOnInit(): void {
-  //   this.activatedRoute.params
-  //     .pipe(
-  //       switchMap(({id}) => this.paisService.getPaisPorAlpha(id)),
-  //       tap(console.log)
-  //     )
-  //     .subscribe(pais => this.pais = pais );
-    // this.activatedRoute.params
-    //   .subscribe(({id}) =>{
-    //     console.log(id);
-
-    //     this.paisService.getPaisPorAlpha(id)
-    //       .subscribe(pais =>{
-    //         console.log(pais);
-            
-    //       });
-
-    //   });
-    
-  // }
-
+  getPaisPorAlpha(cca2: string) {
+    this.paisService.getPaisPorAlpha(cca2).subscribe((data: any) => {
+      this.pais = data[0];
+    });
+  }
 }
